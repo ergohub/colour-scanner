dc47963c02048459d6121894ab3bd7111c5a0d6d
Derek Stanley <nebari@dereksmudio1387.home>
2024-02-23 11:52:55 +0000

    convert numbers to backgound styles

--- src/components/Imageholder/Imageholder.js
+++ src/components/Imageholder/Imageholder.js
@@ -14,7 +14,7 @@ const Imageholder = ({imageUrl, colours}) => {
             </Row>
             <Row>
                 {colours.map((colour, id) => (
-                    <Col key={id}>{colour[id]}</Col>
+                    <Col key={id}>{colour}</Col>
                 ))}
             </Row>
         </Container>
